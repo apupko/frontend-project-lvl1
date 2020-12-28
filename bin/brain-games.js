b2b73732a341cb358f72e4e332ad86e66b46d468
Alexandr Pupko <pupko.alexander@gmail.com>
2020-12-28 15:20:15 +0600

    Fix formating

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import readline from './src/cli.js';
+import readline from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const name = readline();
-console.log(`Hello, ${name}`);
+console.log(`Hello, ${name}!`);
