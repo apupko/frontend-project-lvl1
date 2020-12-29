34f8a68af4148bbbcf371cfc91ed43e9facd3f68
Alexandr Pupko <pupko.alexander@gmail.com>
2020-12-29 09:14:04 +0600

    Refactoring code

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-import readline from '../src/cli.js';
+import welcome from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-const name = readline();
-console.log(`Hello, ${name}!`);
+welcome();
